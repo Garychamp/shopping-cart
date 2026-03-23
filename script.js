3ddb7770ab2e8f31b76759eedb0234466eb3de59
Garychamp <garyhoare18@gmail.com>
2026-03-23 19:31:35 +0000

    Add buttons to increase quantity and remove items in the shopping cart

--- script.js
+++ script.js
@@ -10,7 +10,26 @@ function showCart() {
 
   cart.forEach((item) => {
     const li = document.createElement("li");
-    li.textContent = `${item.name} - €${item.price} x ${item.quantity}`;
+    li.textContent = `${item.name} - €${item.price} x ${item.quantity} `;
+
+    // Button to increase quantity
+    const addBtn = document.createElement("button");
+    addBtn.textContent = "+";
+    addBtn.onclick = () => {
+      item.quantity += 1;
+      updateCart();
+    };
+
+    // Button to remove item
+    const removeBtn = document.createElement("button");
+    removeBtn.textContent = "Remove";
+    removeBtn.onclick = () => {
+      removeItem(item.name);
+      updateCart();
+    };
+
+    li.appendChild(addBtn);
+    li.appendChild(removeBtn);
     cartList.appendChild(li);
   });
 }
@@ -20,6 +39,13 @@ showCart();
 
 function getTotal() {
   return cart.reduce((sum, item) => sum + item.price * item.quantity, 0);
+}
+
+function removeItem(itemName) {
+  const index = cart.findIndex((item) => item.name === itemName);
+  if (index !== -1) {
+    cart.splice(index, 1);
+  }
 }
 
 function updateCart() {
